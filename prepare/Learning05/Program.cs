4fe9abe224c4f2dc13e1626e8683bdcab8eb8a2e
cortecer012 <bigjulio12.10@gmail.com>
2023-04-26 21:51:48 -0500

    Added prep 5 activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        DisplayWelcome();
+        PromptUserNumber();
+        int number = 43;
+        int squaredNumber = number * number;
+        string userName = PromptUserName();
+        DisplayResult(userName, squaredNumber);
     }
+    static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the Program!");
+        }
+
+    static string PromptUserName()
+        {
+            Console.Write("Please enter your userName: ");
+            string userName = Console.ReadLine();
+            return userName;
+        }
+
+    static int PromptUserNumber()
+        {
+            Console.Write("Please enter your favorite number: ");
+            string userInput = Console.ReadLine();
+            int number = int.Parse(userInput);
+            return number;
+        }
+
+    static void DisplayResult(string userName, int squaredNumber)
+        {
+            Console.WriteLine($"{userName}, the square of your number is: {squaredNumber}");
+        }
+
+        
+          
+
+
+        
+
+    
 }
